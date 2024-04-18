075b86b9d29859d0f100eee8991607f5ff5e2382
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-18 02:07:49 +0200

    Convertir los paìses y mostrarlos en pantalla

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Country, Region, SmallCountry } from '../interfaces/country.interface';
-import { Observable, of, tap } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -18,7 +18,17 @@ export class CountriesService {
 
 	getCountriesByRegion(region: Region): Observable<SmallCountry[]> {
 		if (!region) return of([]);
+
 		const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
-		return this.http.get<SmallCountry[]>(url).pipe(tap((response) => console.log({ response })));
+
+		return this.http.get<Country[]>(url).pipe(
+			map((countries) =>
+				countries.map((country) => ({
+					name: country.name.common,
+					cca3: country.cca3,
+					borders: country.borders ?? [],
+				}))
+			)
+		);
 	}
 }
