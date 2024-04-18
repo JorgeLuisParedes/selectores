51f11b784e81f5925103918eff3b71465a3882a4
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-18 02:35:04 +0200

    Tercer selector anidado - Fronteras

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interface';
-import { switchMap, tap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
 	selector: 'app-selector-page',
@@ -11,16 +11,18 @@ import { switchMap, tap } from 'rxjs';
 })
 export class SelectorPageComponent implements OnInit {
 	public countriesByRegion: SmallCountry[] = [];
+	public borders: string[] = [];
 	public myForm: FormGroup = this.fb.group({
 		region: ['', Validators.required],
 		country: ['', Validators.required],
-		borders: ['', Validators.required],
+		border: ['', Validators.required],
 	});
 
 	constructor(private fb: FormBuilder, private countriesService: CountriesService) {}
 
 	ngOnInit(): void {
 		this.onRegionChanged();
+		this.onCountryChange();
 	}
 
 	get regions(): Region[] {
@@ -32,10 +34,25 @@ export class SelectorPageComponent implements OnInit {
 			.get('region')!
 			.valueChanges.pipe(
 				tap(() => this.myForm.get('country')!.setValue('')),
+				tap(() => (this.borders = [])),
 				switchMap((region) => this.countriesService.getCountriesByRegion(region))
 			)
 			.subscribe((countries) => {
 				this.countriesByRegion = countries;
+			});
+	}
+
+	onCountryChange(): void {
+		this.myForm
+			.get('country')!
+			.valueChanges.pipe(
+				tap(() => this.myForm.get('border')!.setValue('')),
+				filter((value: string) => value.length > 0),
+				switchMap((alphaCode) => this.countriesService.getCountryByAlphaCode(alphaCode))
+			)
+			.subscribe((country) => {
+				console.log({ borders: country.borders });
+				this.borders = country.borders;
 			});
 	}
 }
